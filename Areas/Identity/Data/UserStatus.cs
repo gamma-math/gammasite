29b78aeaa4eebb1f61efaa3c8a3514a69f865211
Michael Staal-Olsen <m@mstaal.com>
2020-07-24 18:08:57 +0200

    Add project to Github

---
+++ Areas/Identity/Data/UserStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamMaSite.Areas.Identity.Data
+{
+    public enum UserStatus
+    {
+        OPRETTET,
+        BETALT,
+        SKYLDER,
+        INAKTIV
+    }
+}
---
+++ Models/GamMaUser.cs
@@ -0,0 +1,21 @@
+﻿using GamMaSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace GamMaSite.Models
+{
+    public class GamMaUser : IdentityUser
+    {
+        [PersonalData]
+        public string Navn { get; set; }
+
+        [PersonalData]
+        public string Adresse { get; set; }
+
+        [PersonalData]
+        public string Aargang { get; set; }
+
+        [PersonalData]
+        public UserStatus Status { get; set; }
+
+    }
+}
