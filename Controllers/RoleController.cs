57bf49e354b290e354a4908491b5f34770839a1e
Michael Staal-Olsen <m@mstaal.com>
2020-10-10 17:17:43 +0200

    Tilføj mulighed for at tilføje og redigere roller

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GamMaSite.Areas.Identity.Data;
+using GamMaSite.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamMaSite.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<GamMaUser> userManager;
+        public RoleController(RoleManager<IdentityRole> roleMgr, UserManager<GamMaUser> userMrg)
+        {
+            roleManager = roleMgr;
+            userManager = userMrg;
+        }
+
+        public ViewResult Index() => View(roleManager.Roles);
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(name);
+                IdentityResult result = await roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "No role found");
+            return View("Index", roleManager.Roles);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<GamMaUser> members = new List<GamMaUser>();
+            List<GamMaUser> nonMembers = new List<GamMaUser>();
+            foreach (GamMaUser user in userManager.Users.ToList())
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEdit
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(RoleModification model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.AddIds ?? new string[] { })
+                {
+                    GamMaUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+                foreach (string userId in model.DeleteIds ?? new string[] { })
+                {
+                    GamMaUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+            else
+                return await Update(model.RoleId);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError(error.Code, error.Description);
+        }
+    }
+}
