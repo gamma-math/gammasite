1e5be005edf3f1858ebf68280cd90e10536fb786
Michael Staal-Olsen <m@mstaal.com>
2020-10-11 23:48:19 +0200

    Første take på en medlemsoversigt

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamMaSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamMaSite.Controllers
+{
+    
+    public class UsersController : Controller
+    {
+        private UserManager<GamMaUser> userManager;
+
+        public UsersController(UserManager<GamMaUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
