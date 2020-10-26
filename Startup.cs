3c73450d9a254bdcf550d73ac0b47aa0576bac03
Michael Staal-Olsen <m@mstaal.com>
2020-10-27 00:03:25 +0100

    Make URL links lower case

--- Startup.cs
+++ Startup.cs
@@ -63,6 +63,7 @@ namespace GamMaSite
             );
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddRouting(options => { options.LowercaseUrls = true; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
