1d36261fc237568723817a34326f2a0568fdeb4f
Michael Staal-Olsen <m@mstaal.com>
2020-11-07 23:00:05 +0100

    First draft towards configurationService

---
+++ Services/ConfigurationService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+
+namespace GamMaSite.Services
+{
+    public class ConfigurationService
+    {
+        private static ConfigurationService _configurationService = new ConfigurationService();
+
+        private ConcurrentDictionary<string, byte[]> configDictionary = new ConcurrentDictionary<string, byte[]>();
+
+        public static ConfigurationService GetInstance()
+        {
+            return _configurationService;
+        }
+
+        public string Get(string key)
+        {
+            return Encoding.UTF8.GetString(GetBytes(key));
+        }
+
+        public Stream GetStream(string key)
+        {
+            return new MemoryStream(GetBytes(key));
+        }
+
+        public XslCompiledTransform GetTransform(string key)
+        {
+            XElement element = XElement.Parse(Get(key));
+            XslCompiledTransform xsltTransformation = new XslCompiledTransform(false);
+            xsltTransformation.Load(element.CreateReader(), new XsltSettings(false, true), new ConfigXmlResolver());
+            return xsltTransformation;
+        }
+
+        public byte[] GetBytes(string key)
+        {
+            return configDictionary[key];
+        }
+
+        public void Redefine(byte[] zipAsBytes)
+        {
+            lock(configDictionary)
+            {
+                configDictionary.Clear();
+                var zip = new ZipService().GetZipAsMap(zipAsBytes);
+                foreach (string key in zip.Keys)
+                {
+                    configDictionary[key] = zip[key];
+                }
+            }
+        }
+    }
+
+    public class ConfigXmlResolver : XmlUrlResolver
+    {
+        private ConfigurationService _configurationService = ConfigurationService.GetInstance();
+
+        public ConfigXmlResolver() : base()
+        {
+        }
+
+        public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+        {
+            return _configurationService.GetStream(absoluteUri.ToString());
+        }
+    }
+}
---
+++ Services/ZipService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace GamMaSite.Services
+{
+    public class ZipService
+    {
+        public Dictionary<string, byte[]> GetZipAsMap(Stream stream)
+        {
+            Dictionary<string, byte[]> dictionary = new Dictionary<string, byte[]>();
+            ZipArchive zis = new ZipArchive(stream);
+            ReadOnlyCollection<ZipArchiveEntry> entries = zis.Entries;
+            foreach (ZipArchiveEntry entry in entries)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    entry.Open().CopyTo(memoryStream);
+                    dictionary[Path.GetFullPath(entry.FullName)] = memoryStream.ToArray();
+                }
+            }
+            return dictionary;
+        }
+
+        public Dictionary<string, byte[]> GetZipAsMap(byte[] bytes)
+        {
+            var stream = new MemoryStream(bytes);
+            var dictionary = GetZipAsMap(stream);
+            return dictionary;
+        }
+    }
+}
