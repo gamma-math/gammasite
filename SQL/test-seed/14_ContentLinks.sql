3484156b66c4c38146312441f7ed97174c0041f5
AbrahimBorgiPrivat <Abrahim.borgi@live.dk>
2026-09-03 20:23:56 +0200

    Update test seed data and prepare TEST deployment

--- SQL/test-seed/14_ContentLinks.sql
+++ SQL/test-seed/14_ContentLinks.sql
@@ -3,7 +3,9 @@ SET NAMES utf8mb4;
 DELETE FROM `ContentLinks`;
 
 INSERT INTO `ContentLinks` (`ContentItemId`, `Label`, `Url`, `Type`, `SortOrder`, `Created`, `Updated`) VALUES
-((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'karriere-efter-matematik' LIMIT 1), 'LinkedIn event', 'https://www.linkedin.com/company/gamma-math-ucph/', 'LINKEDIN', 0, '2026-08-20 10:05:00', '2026-08-20 10:05:00'),
-((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'karriere-efter-matematik' LIMIT 1), 'Find vej', 'https://google.com/maps?q=Universitetsparken%205%202100%20K%C3%B8benhavn', 'OTHER', 1, '2026-08-20 10:05:00', '2026-08-20 10:05:00'),
-((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'gamma-julebanko-2026' LIMIT 1), 'Facebook-gruppen', 'https://www.facebook.com/share/g/1Exv7epudc/', 'FACEBOOK', 0, '2026-08-22 12:05:00', '2026-08-22 12:05:00'),
+((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'bastard-fyraftenscafe-2026' LIMIT 1), 'Facebook-event', 'https://www.facebook.com/events/935100782602251', 'FACEBOOK', 0, '2026-04-15 10:05:00', '2026-04-15 10:05:00'),
+((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'faglig-aften-juni-2026' LIMIT 1), 'Facebook-event', 'https://www.facebook.com/events/1352011586978094/', 'FACEBOOK', 0, '2026-04-20 10:05:00', '2026-04-20 10:05:00'),
+((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'sommer-bingo-banko-fest-2026' LIMIT 1), 'Facebook-event', 'https://facebook.com/events/s/gamma-sommer-bingo-banko-fest/1731919154816779/', 'FACEBOOK', 0, '2026-07-10 09:35:00', '2026-07-10 09:35:00'),
+((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'koebenhavnermuren-2026' LIMIT 1), 'Facebook-event', 'https://www.facebook.com/events/1528726255308131/', 'FACEBOOK', 0, '2026-08-15 10:05:00', '2026-08-15 10:05:00'),
+((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'koebenhavnermuren-2026' LIMIT 1), 'Tilmeld dig - betal 50 kr.', 'https://mobilepay.dk/erhverv/betalingslink/betalingslink-svar?phone=22766&amount=50&comment=K%C3%B8benhavnermuren', 'PAYMENT', 1, '2026-08-15 10:05:00', '2026-08-15 10:05:00'),
 ((SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'react-migrationen-er-i-gang' LIMIT 1), 'Læs projektstatus', 'https://github.com/gamma-math', 'OTHER', 0, '2026-08-26 08:50:00', '2026-08-26 08:50:00');
--- SQL/test-seed/15_EventRegistrations.sql
+++ SQL/test-seed/15_EventRegistrations.sql
@@ -4,29 +4,38 @@ DELETE FROM `EventRegistrations`;
 
 INSERT INTO `EventRegistrations` (`ContentItemId`, `UserId`, `RegistrationType`, `Registered`, `ResponseText`, `Created`, `Updated`) VALUES
 (
-  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'karriere-efter-matematik' LIMIT 1),
+  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'bastard-fyraftenscafe-2026' LIMIT 1),
   (SELECT `Id` FROM `AspNetUsers` WHERE `Email` = 'bruger.test@gamma.local' LIMIT 1),
   'ATTENDEE',
   1,
-  'Jeg glæder mig til at høre om data science-sporet.',
-  '2026-08-21 09:15:00',
-  '2026-08-21 09:15:00'
+  'Kommer forbi til en øl og et spil.',
+  '2026-04-16 09:15:00',
+  '2026-04-16 09:15:00'
 ),
 (
-  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'karriere-efter-matematik' LIMIT 1),
+  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'faglig-aften-juni-2026' LIMIT 1),
   (SELECT `Id` FROM `AspNetUsers` WHERE `Email` = 'admin.test@gamma.local' LIMIT 1),
-  'HOST',
+  'ORGANIZER',
   1,
-  'Tager imod oplægsholdere.',
-  '2026-08-21 09:20:00',
-  '2026-08-21 09:20:00'
+  'Hjælper med mad og oplægsholdere.',
+  '2026-04-21 09:20:00',
+  '2026-04-21 09:20:00'
 ),
 (
-  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'gamma-julebanko-2026' LIMIT 1),
+  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'sommer-bingo-banko-fest-2026' LIMIT 1),
   (SELECT `Id` FROM `AspNetUsers` WHERE `Email` = 'bruger.test@gamma.local' LIMIT 1),
   'ATTENDEE',
   1,
-  'Kommer med godt bankohumør.',
-  '2026-08-23 18:30:00',
-  '2026-08-23 18:30:00'
+  'Tager bankohumør med.',
+  '2026-07-11 18:30:00',
+  '2026-07-11 18:30:00'
+),
+(
+  (SELECT `Id` FROM `ContentItems` WHERE `Slug` = 'koebenhavnermuren-2026' LIMIT 1),
+  (SELECT `Id` FROM `AspNetUsers` WHERE `Email` = 'admin.test@gamma.local' LIMIT 1),
+  'ORGANIZER',
+  1,
+  'Koordinerer ruten.',
+  '2026-08-16 10:15:00',
+  '2026-08-16 10:15:00'
 );
